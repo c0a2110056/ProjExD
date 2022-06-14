2850e0d46a15fe6bfadc10ac99e853ca3e9140ac
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-06-14 16:31:00 +0900

    　コメント #2

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,9 +1,9 @@
 import datetime
 import random
 mondai = ["サザエさんの旦那の名前は？","カツオの妹の名前は","タラオはカツオから見てどんな関係？"]
-kotae = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
-n = random.randint(0,3)
-#お褒めいただき光栄です
+answer = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
+n = random.randint(0,2)
+#お褒めいただき光栄ですw
 def shutudai():
     print("問題")
     print(mondai[n])
@@ -11,7 +11,7 @@ def shutudai():
 
 def kaito():
     ans =input("答え：")
-    if ans in kotae[n]:
+    if ans in answer[n]:
         print("正解！！！")
     else:
         print("出直してこい")
