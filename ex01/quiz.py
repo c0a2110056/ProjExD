c670e0e1b0a9a5db6743d9e8f21a9df32a7b7882
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-06-14 16:17:51 +0900

    　コメント #1

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -3,7 +3,7 @@ import random
 mondai = ["サザエさんの旦那の名前は？","カツオの妹の名前は","タラオはカツオから見てどんな関係？"]
 kotae = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
 n = random.randint(0,3)
-
+#お褒めいただき光栄です
 def shutudai():
     print("問題")
     print(mondai[n])
