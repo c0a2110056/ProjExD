62c150fc1c6e092714910849e88c27031bdf6a8b
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-06-21 14:57:54 +0900

    　5問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     root = tk.Tk()
     root.title("calc")
-    root.geometry("300x500")
+    #root.geometry("300x500")
 
     entry = tk.Entry(root, justify="right",width=10,font=("Times New Roman", 40))
     entry.grid(row=0,column=0,columnspan=3)
@@ -15,17 +15,18 @@ if __name__ == "__main__":
     def button_click(event):
         btn = event.widget
         txt = btn["text"]
-        tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
-        
+        #tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
+        entry.insert(tk.END,txt)
 
     r,c = 1,0 #行番号r,列番号cの設定
     for i in range(9,-1,-1):
         
-        button = tk.Button(root, text=i, width=4, height=2,
+        button = tk.Button(root, text=f"{i}", width=4, height=2,
                             font=("Times New Roman", 30),
-                            command=button_click)
+                            )
         button.bind("<1>",button_click)
         button.grid(row = r,column = c)
+
         c += 1
         if (i-1)%3==0:
             r +=1
