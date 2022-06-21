1a43a1ec5908ec22ef825878026fa688af5271ab
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-06-21 14:47:36 +0900

    　4問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+from turtle import right
 
 if __name__ == "__main__":
 
@@ -7,12 +8,17 @@ if __name__ == "__main__":
     root.title("calc")
     root.geometry("300x500")
 
+    entry = tk.Entry(root, justify="right",width=10,font=("Times New Roman", 40))
+    entry.grid(row=0,column=0,columnspan=3)
+    
+
     def button_click(event):
         btn = event.widget
         txt = btn["text"]
         tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
+        
 
-    r,c = 0,0 #行番号r,列番号cの設定
+    r,c = 1,0 #行番号r,列番号cの設定
     for i in range(9,-1,-1):
         
         button = tk.Button(root, text=i, width=4, height=2,
@@ -24,5 +30,6 @@ if __name__ == "__main__":
         if (i-1)%3==0:
             r +=1
             c=0
-        
+    
+    
     root.mainloop()
