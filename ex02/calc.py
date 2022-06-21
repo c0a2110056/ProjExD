908be04495c72b4bc4b947d3bc004c379f2a264e
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-06-21 14:28:40 +0900

    　3問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,14 +7,18 @@ if __name__ == "__main__":
     root.title("calc")
     root.geometry("300x500")
 
+    def button_click(event):
+        btn = event.widget
+        txt = btn["text"]
+        tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
+
     r,c = 0,0 #行番号r,列番号cの設定
     for i in range(9,-1,-1):
         
         button = tk.Button(root, text=i, width=4, height=2,
                             font=("Times New Roman", 30),
-                            command=button_click
-                            )
-        
+                            command=button_click)
+        button.bind("<1>",button_click)
         button.grid(row = r,column = c)
         c += 1
         if (i-1)%3==0:
