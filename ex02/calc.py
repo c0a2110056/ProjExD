2b462cb273efd8af1b2fe02085ad8c5d95b9043e
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-06-21 14:12:40 +0900

    　1問目

---
+++ ex02/calc.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+if __name__ == "__main__":
+    
+    root = tk.Tk()
+    root.title("calc")
+    root.geometry("300x500")
+    root.mainloop()
