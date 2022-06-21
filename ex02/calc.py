4bec273423fe442a6daf4a13883d383adde9f596
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-06-21 14:25:54 +0900

    　2問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,8 +2,23 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 if __name__ == "__main__":
-    
+
     root = tk.Tk()
     root.title("calc")
     root.geometry("300x500")
+
+    r,c = 0,0 #行番号r,列番号cの設定
+    for i in range(9,-1,-1):
+        
+        button = tk.Button(root, text=i, width=4, height=2,
+                            font=("Times New Roman", 30),
+                            command=button_click
+                            )
+        
+        button.grid(row = r,column = c)
+        c += 1
+        if (i-1)%3==0:
+            r +=1
+            c=0
+        
     root.mainloop()
