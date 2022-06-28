e0593aee81a5bbf85c647918a714d4af5c01ab19
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-06-28 14:26:46 +0900

    　3問目

--- ex03/maze_maker.py
+++ ex03/maze_maker.py
@@ -38,4 +38,7 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(width=1500,height=900,bg="black")
     canvas.pack()
+    tori = tk.PhotoImage(file="fig/9.png")
+    cx,cy = 300,400
+    canvas.create_image(cx,cy,image=tori,tag="tori")
     root.mainloop()   
