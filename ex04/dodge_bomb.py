6bc33ac29539a118c29576081686275691750d26
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-07-05 14:35:23 +0900

    　4問目

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -19,11 +19,17 @@ def main():
 
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
-        screen_sfc.blit(kkimg_sfc,kkimg_rct)
-
+        
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
+
+        key_states = pg.key.get_pressed() #辞書
+        if key_states[pg.K_UP]    == True: kkimg_rct.centery -= 1
+        if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
+        if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
+        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+        screen_sfc.blit(kkimg_sfc,kkimg_rct)
+
         pg.display.update()
         clock.tick(1000)
 
