a7630f623554cb23f7af6b320450644bc3b732c6
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-07-05 14:48:46 +0900

    　5問目

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,9 +1,11 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock() #　時間計測用のオブジェクト
 
+    #　スクリーンと背景
     pg.display.set_caption("逃げろ！こうかとん")   #　タイトルバーに表示
     screen_sfc = pg.display.set_mode((1600,900)) #　1600x900の画面Surfaceを生成する
     screen_rct = screen_sfc.get_rect()           #　Rect
@@ -11,12 +13,19 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()             #　Rect
     #screen_sfc.blit(bgimg_sfc,bgimg_rct)
 
+    #　こうかとん
     kkimg_sfc = pg.image.load("fig/6.png") #　Surface
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc,0,2.0) #　Surface
     kkimg_rct = kkimg_sfc.get_rect()       #　Rect
     kkimg_rct.center = 900, 400
 
-
+    #　爆弾
+    bmimg_sfc = pg.Surface((20,20)) #　Surface
+    bmimg_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bmimg_sfc,(255,0,0),(10,10),10)
+    bmimg_rct = bmimg_sfc.get_rect() #　Rect
+    bmimg_rct.centerx = random.randint(0,screen_rct.width)
+    bmimg_rct.centerx = random.randint(0,screen_rct.height)
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
         
@@ -29,6 +38,9 @@ def main():
         if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
+
+        #　爆弾
+        screen_sfc.blit(bmimg_sfc,bmimg_rct)
 
         pg.display.update()
         clock.tick(1000)
