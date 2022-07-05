524957eb51d9f64e42124d5118ac64d78b2348d7
nemoyuu <c0a2110056@edu.teu.ac.jp>
2022-07-05 15:27:58 +0900

    　7問目

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -39,6 +39,13 @@ def main():
         if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
         if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+
+        #　判定
+        if check_bound(kkimg_rct,screen_rct) != (1, 1): #領域外だったら
+            if key_states[pg.K_UP]    == True: kkimg_rct.centery += 1
+            if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -= 1
+            if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx += 1
+            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
 
         #　爆弾の移動
@@ -47,9 +54,22 @@ def main():
         #　爆弾
         screen_sfc.blit(bmimg_sfc,bmimg_rct)
 
+        yoko,tate = check_bound(bmimg_rct, screen_rct)
+        vx *= yoko
+        vy *= tate
+        
         pg.display.update()
         clock.tick(1000)
 
+def check_bound(rct,scr_rct):
+    '''
+    [1] rct: こうかとん　or 爆弾のRect
+    [2] scr_rct: スクリーンのRect
+    '''
+    yoko, tate = +1, +1 # 領域内
+    if rct.left < scr_rct.left or scr_rct.right < rct.right: yoko = -1 #　領域外
+    if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom: tate = -1 #　領域外
+    return yoko, tate
 
 if __name__ == "__main__":
     pg.init()             #　モジュールを初期化する
